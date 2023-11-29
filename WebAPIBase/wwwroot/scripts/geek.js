e3eaa8890f5819748c15a6330f8cb09c12786037
uws0yk <uws0yk@SZA0025883>
2023-11-29 16:18:41 +0100

    tömb feladat

---
+++ WebAPIBase/wwwroot/scripts/geek.js
@@ -0,0 +1,14 @@
+﻿
+
+window.onload = function () {
+    fetch('/jokes.json')
+        .then(response => response.json())
+        .then(data => letöltésBefejeződött(data)
+        );
+}
+
+function letöltésBefejeződött(d) {
+    console.log("Sikeres letöltés")
+    console.log(d)
+    viccek = d;
+}
